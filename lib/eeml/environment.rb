b4101516bc14bf435bb8e9e53afb579df15899ae
James Smith <james@floppy.org.uk>
2008-08-22 18:08:10 +0100

    Add documentation

--- lib/eeml/environment.rb
+++ lib/eeml/environment.rb
@@ -1,7 +1,10 @@
 module EEML
 
+  # An EEML environment object. Contains a number of EEML::Data objects amongst
+  # other attributes.
   class Environment
 
+    # Convert to EEML
     def to_eeml
       raise EEML::NoData.new('EEML requires at least one data item')
     end
